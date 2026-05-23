26699d9338807eb9261c534caede1c0ce996345a
xOagge <jose.goncalo2004@gmail.com>
2026-05-23 19:32:23 +0100

    hell yea

---
+++ Clean_Code_1/Helper_Mandatory.py
@@ -0,0 +1,6 @@
+from slitherlink import *
+
+#declara so a Slitherlink, e se o print nao estiver comentado mostra as edges obrigatorias
+board = Board.parse_instance()
+
+problem = Slitherlink(board)
